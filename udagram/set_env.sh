8d753a57a6f5dfadca01581b0c63a7759a6f332e
QuangHa797979 <qngha7@gmail.com>
2023-03-26 10:37:20 +0700

    first commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=adminadmin
+export POSTGRES_HOST=database-1.c6wvjaj61un3.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=quangpostgres
+export AWS_BUCKET=arn:aws:s3:::myquangbucket
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InFuZ2hhN0BnbWFpbC5jb20iLCJpYXQiOjE2Nzk3OTUzMzJ9.OxkXMQ8iw7nC_dEDKaVHt88Wv2if20eDhlZ5Zs7MKbI
+export URL=http://localhost:8100
